1023279933958352ca1b0c418428099772a12851
James Weng <wengj@umich.edu>
2022-12-30 13:01:39 -0500

    Cythonize output

--- setup.py
+++ setup.py
@@ -11,30 +11,26 @@ if sys.platform == "darwin":
     cxx_flags.append("-DGGML_USE_ACCELERATE")
     ld_flags.extend(["-framework", "Accelerate"])
 
-    os.environ['CFLAGS']   = '-DGGML_USE_ACCELERATE -O3 -std=c11'
-    os.environ['LDFLAGS']  = '-framework Accelerate'
+    os.environ["CFLAGS"] = "-DGGML_USE_ACCELERATE -O3 -std=c11"
+    os.environ["LDFLAGS"] = "-framework Accelerate"
 else:
     cxx_flags.extend(["-mavx", "-mavx2", "-mfma", "-mf16c"])
-    
-    os.environ['CFLAGS']   = '-mavx -mavx2 -mfma -mf16c -O3 -std=c11'
 
-cythonize("whispercpp.pyx")
+    os.environ["CFLAGS"] = "-mavx -mavx2 -mfma -mf16c -O3 -std=c11"
 
-external_modules = [
-    Extension(
-        name="whispercpp.pyx",
-        sources=["whispercpp.cpp", "whisper.cpp/whisper.cpp"],
-        extra_compile_args=cxx_flags,
-        extra_link_args=ld_flags,
-    ),
-]
+module = Extension(
+    name="whispercpp",
+    sources=["whispercpp.pyx", "whisper.cpp/whisper.cpp"],
+    extra_compile_args=cxx_flags,
+    extra_link_args=ld_flags,
+)
 
 static_libs = [
     ("ggml", {"sources": ["whisper.cpp/ggml.c"]}),
 ]
 
 setup(
-    ext_modules=external_modules,
+    ext_modules=cythonize(module),
     libraries=static_libs,
     include_dirs=["whisper.cpp/", numpy.get_include()],
     zip_safe=False,
